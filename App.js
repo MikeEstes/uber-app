3aa89958a772ee5e6b3e9ae30f201c9507e82c1d
Mike Estes <sregge@gmail.com>
2021-06-15 23:22:53 -0400

    Initial repo creation

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+    </>
+  );
+};
+
+export default App;
